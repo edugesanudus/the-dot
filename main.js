a299a0b645d7c62cc41c9e8ccf930ff34180e5bf
edugesanudus <69233218+edugesanudus@users.noreply.github.com>
2024-04-07 15:47:25 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+	document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+const clip = document.querySelectorAll(".hover-to-play");
+for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", function (e) { clip[i].play();
+  }); clip[i].addEventListener("mouseout", function (e) { clip[i].pause(); }); }
