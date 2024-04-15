2b615c31a8d4d2f308e672d6f6a42ee258bae4d7
edugesanudus <69233218+edugesanudus@users.noreply.github.com>
2024-04-15 11:25:29 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -5,3 +5,8 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
 const clip = document.querySelectorAll(".hover-to-play");
 for (let i = 0; i < clip.length; i++) { clip[i].addEventListener("mouseenter", function (e) { clip[i].play();
   }); clip[i].addEventListener("mouseout", function (e) { clip[i].pause(); }); }
+
+
+/* import { Ripple, initMDB } from "mdb-ui-kit";
+
+initMDB({ Ripple }); */
